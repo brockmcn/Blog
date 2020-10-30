c115fd9afaf28f2f736431276daa3a370041ade6
Brock <brockmcn@gmail.com>
2020-10-30 00:49:48 -0400

    blog 7

--- pal/src/calculator.js
+++ pal/src/calculator.js
@@ -2,17 +2,22 @@ import React from 'react'
 
 export function Calculator() {
     return (
-        <div className="digit">
-            <div id="0">0</div>
-            <div id="1">1</div>
-            <div id="2">2</div>
-            <div id="3">3</div>
-            <div id="4">4</div>
-            <div id="5">5</div>
-            <div id="6">6</div>
-            <div id="7">7</div>
-            <div id="8">8</div>
-            <div id="9">9</div>
+        <div id="calculator">
+            <textarea></textarea>
+            <div id="digit">
+                <button onClick={() => { console.log("0"); }}>0</button>
+                <button onClick={() => { console.log("1"); }}>1</button>
+                <button onClick={() => { console.log("2"); }}>2</button>
+                <button onClick={() => { console.log("3"); }}>3</button>
+                <button onClick={() => { console.log("4"); }}>4</button>
+                <button onClick={() => { console.log("5"); }}>5</button>
+                <button onClick={() => { console.log("6"); }}>6</button>
+                <button onClick={() => { console.log("7"); }}>7</button>
+                <button onClick={() => { console.log("8"); }}>8</button>
+                <button onClick={() => { console.log("9"); }}>9</button>
+                <button onClick={() => { console.log("+"); }}>+</button>
+                <button onClick={() => { console.log("="); }}>=</button>
+            </div>
         </div>
     );
 }
