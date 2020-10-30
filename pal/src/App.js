825d4e93eb7b1687c63b6392af197f9c87de3f95
Brock <brockmcn@gmail.com>
2020-10-29 22:44:55 -0400

    react stuffs

---
+++ pal/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css';
+
+import {Calculator} from './calculator.js'
+
+function App() {
+  return (
+    <div id="blog">
+      <Calculator />
+    </div>
+  );
+}
+
+export default App;
